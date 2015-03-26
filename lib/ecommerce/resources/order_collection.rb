f1136385df340786e2956328d76187438f56212f
Eduardo Hertz <eduardohertz@gmail.com>
2015-03-26 12:57:53 -0300

    Adds pagination methods to Ecommerce::Resources::OrderCollection

--- lib/ecommerce/resources/order_collection.rb
+++ lib/ecommerce/resources/order_collection.rb
@@ -1,11 +1,15 @@
 module Ecommerce
   module Resources
     class OrderCollection < Base
-      attr_reader :response, :orders
+
+      PAGE_REGEX = /page=(\d+)/
+
+      attr_reader :response, :orders, :headers
 
       def initialize(response)
         @response = response
         @orders = []
+        @headers = response.headers['Link'].split(',')
       end
 
       def self.build(response)
@@ -13,11 +17,40 @@ module Ecommerce
       end
 
       def build
+        build_orders
+        self
+      end
+
+      def next_page
+        page_for(:next)
+      end
+
+      def last_page
+        page_for(:last)
+      end
+
+      def previous_page
+        page_for(:prev)
+      end
+
+      def first_page
+        page_for(:first)
+      end
+
+      private
+
+      def page_for(page_rel)
+        header_link_for(page_rel).match(PAGE_REGEX)[1].to_i rescue nil
+      end
+
+      def header_link_for(rel)
+        headers.select{|n| n =~ /rel=#{rel}/}.first
+      end
+
+      def build_orders
         Ecommerce::Resources::Base.parsed_body(response).each do |order_attributes|
           orders.push(Ecommerce::Resources::Order.new(order_attributes))
         end
-
-        self
       end
     end
   end
