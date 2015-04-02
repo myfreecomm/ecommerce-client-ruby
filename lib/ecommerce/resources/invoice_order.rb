07a8a382cb46ad331c10605cdcea2122a11e2b2a
Eduardo Hertz <eduardohertz@gmail.com>
2015-04-02 17:30:28 -0300

    Create Ecommerce::Resources::InvoiceOrder#find

--- lib/ecommerce/resources/invoice_order.rb
+++ lib/ecommerce/resources/invoice_order.rb
@@ -13,6 +13,7 @@ module Ecommerce
       attr_reader :redeemed_adjustments, :order_url, :url, :created_at, :activated_at, :number, :user_data,
                   :amount_with_adjustments, :amount, :paid, :payment_method, :plan_data, :charge_date, 
                   :next_payment_date
+      
       #
       # Lists all invoice of an orders and return a collection of invoice with pagination information (represented by Ecommerce::Resources::InvoiceCollection)
       #
@@ -26,6 +27,28 @@ module Ecommerce
         client.get("/api/orders/#{slug}/#{order_id}/invoices/", { body: { page: page, limit: limit } }) do |response|
           Ecommerce::Resources::InvoiceOrderCollection.build(response)
         end
+      end
+
+      #
+      # Finds an invoice of an orders
+      #
+      # [API]
+      #   Method: <tt>GET /api/accounts/:account_uuid/orders/</tt>
+      #
+      #   Documentation: http://myfreecomm.github.io/passaporte-web/ecommerce/api/orders.html#listagem-das-ordens-de-compra-de-uma-conta
+      #
+
+      def self.find(invoice_order_id, order_id, slug)
+        client.get("/api/orders/#{slug}/#{order_id}/invoices/#{invoice_order_id}/") do |response|
+          build_invoice(response)
+        end
+      end
+
+      private
+
+      def self.build_invoice(response)
+        invoice_attributes = parsed_body(response)
+        invoice_attributes.empty? ? {} : self.new(invoice_attributes)
       end
     end
   end
